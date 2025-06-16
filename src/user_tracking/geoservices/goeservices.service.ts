23c81d4742e250b3c665ea8f5404616ac3eead6f
anup-sakksh <anup.kumar@sakksh.com>
2025-06-16 12:01:34 +0530

    added the endpoint for the get address using lat long

--- src/user_tracking/geoservices/goeservices.service.ts
+++ src/user_tracking/geoservices/goeservices.service.ts
@@ -2,14 +2,10 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class GeoServices {
-
-
     async addressSeprator(fullAddress) {
         // const fullAddress = "B433, Pocket 1, Sector 14 Dwarka, Dwarka, New Delhi, Delhi, 110078, India";
-
         // Split the address by commas and trim each part
         const parts = fullAddress.split(",").map(part => part.trim());
-
         const result = {
             address: parts.slice(0, parts.length - 4).join(", "), // Everything except last 4
             city: parts[parts.length - 4],
@@ -17,13 +13,12 @@ export class GeoServices {
             pincode: parts[parts.length - 2],
             country: parts[parts.length - 1],
         };
-
         return result
-        // console.log(result);
-
+     
     }
 
     async getLoaction(lat: number, long: number) {
+       
         try {
             const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=AIzaSyDwdLC9Vp90oYz9oXNnz9SboEvuttTRBmQ`;
             const response = await fetch(url, {
@@ -33,7 +28,6 @@ export class GeoServices {
             });
 
             const data = await response.json();
-
             const formatted_address = data.results[0].formatted_address;
             if (data.status === "OK" && data.results.length > 0) {
                 return { address:formatted_address , groupaddress: await this.addressSeprator(formatted_address) };
--- src/user_tracking/userTracking.controller.ts
+++ src/user_tracking/userTracking.controller.ts
@@ -1,10 +1,14 @@
 import { CreateUserTrackingDto } from "./dto/create.userTracking.dto";
 import { UserTrackingService } from "./userTracking.service";
 import { Controller, Post, Body, Get, Query } from "@nestjs/common";
+import { GeoServices } from "./geoservices/goeservices.service";
 
 @Controller('user-tracking')
 export class UserTrackingController {
-    constructor(private readonly userTrackingService: UserTrackingService) {
+    constructor(
+        private readonly userTrackingService: UserTrackingService,
+        private readonly geoServices:GeoServices
+    ) {
 
     }   
 
@@ -26,6 +30,14 @@ export class UserTrackingController {
     @Get('checkInDetails')
     getAllcheckInDetails(@Query('user_id') user_id: string) {
         return this.userTrackingService.getAllcheckInDetails(user_id);
+    }
+    
+
+    @Get('latlongAddress')
+    getAddress( @Query('lat') lat:number , @Query('long') long:number)
+    {
+        console.log(lat ,long)
+        return this.geoServices.getLoaction(lat, long)
     }
 
 }
